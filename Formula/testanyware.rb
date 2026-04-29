dd8529649616d2d5b3d785444b26423a9d5efd51
Antony Blakey <antony.blakey@gmail.com>
2026-04-29 10:01:37 +1000

    testanyware v1.0.2

--- Formula/testanyware.rb
+++ Formula/testanyware.rb
@@ -1,7 +1,7 @@
 class Testanyware < Formula
   desc "GUI testing tool for VMs, using tart, designed for LLM-driven testing"
   homepage "https://github.com/Linkuistics/TestAnyware"
-  version "1.0.1"
+  version "1.0.2"
   license "MIT"
 
   depends_on :macos
@@ -9,8 +9,8 @@ class Testanyware < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/Linkuistics/TestAnyware/releases/download/v1.0.1/testanyware-v1.0.1-aarch64-apple-darwin.tar.xz"
-      sha256 "eea2d1b8714b930be5042156864b976caa47fcd63de31e24c63a47e9fb65bd43"
+      url "https://github.com/Linkuistics/TestAnyware/releases/download/v1.0.2/testanyware-v1.0.2-aarch64-apple-darwin.tar.xz"
+      sha256 "193734af601f39d6122d7e316e1977c0b00016b328098c4112f130cb94960a99"
     end
   end
 
