3d5f8609e0f3fc81bdd0ebb9dce822b74ded218a
Antony Blakey <antony.blakey@gmail.com>
2026-05-01 16:00:46 +1000

    atlas v1.2.0

--- Formula/atlas.rb
+++ Formula/atlas.rb
@@ -1,28 +1,28 @@
 class Atlas < Formula
   desc "Design recovery and architectural pattern extraction for large codebases"
   homepage "https://github.com/linkuistics/Atlas"
-  version "1.1.0"
+  version "1.2.0"
   license "Apache-2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/linkuistics/Atlas/releases/download/v1.1.0/atlas-v1.1.0-aarch64-apple-darwin.tar.xz"
-      sha256 "c3d607e684a8111e1b1f41393ced3b4fadc016766e088e8ad70fc68fff6118b2"
+      url "https://github.com/linkuistics/Atlas/releases/download/v1.2.0/atlas-v1.2.0-aarch64-apple-darwin.tar.xz"
+      sha256 "ff0b13061a2131000560e88081c5a72a1bbef1013ab16bddfaf64ff5861e1ab7"
     end
     on_intel do
-      url "https://github.com/linkuistics/Atlas/releases/download/v1.1.0/atlas-v1.1.0-x86_64-apple-darwin.tar.xz"
-      sha256 "44a3ef987e8f6665bbf3371b83b15ca7d8b3e13b0021a0594edcc96bc9da3e5c"
+      url "https://github.com/linkuistics/Atlas/releases/download/v1.2.0/atlas-v1.2.0-x86_64-apple-darwin.tar.xz"
+      sha256 "2d895df40507a756657a15454167593d78c082f0e520b75805e055184d85bfb0"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/linkuistics/Atlas/releases/download/v1.1.0/atlas-v1.1.0-aarch64-unknown-linux-gnu.tar.xz"
-      sha256 "1b60dace436b3a58f2d2dde363f3374616d62dda5a526952e5796dacc73c0f5b"
+      url "https://github.com/linkuistics/Atlas/releases/download/v1.2.0/atlas-v1.2.0-aarch64-unknown-linux-gnu.tar.xz"
+      sha256 "2d638e01e81d7198e6fb5b74ba18bdc164bfb16bd008ca49981c341a5c0a30fa"
     end
     on_intel do
-      url "https://github.com/linkuistics/Atlas/releases/download/v1.1.0/atlas-v1.1.0-x86_64-unknown-linux-gnu.tar.xz"
-      sha256 "0dfb61efba07ff06a4de4d52eb9662f27203ec8a4da3917fa5b39e3a3b893bc3"
+      url "https://github.com/linkuistics/Atlas/releases/download/v1.2.0/atlas-v1.2.0-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "42e26eddef42e0fb8c0fe72d512aa1528aa10adc89d597d3da05fea55e544fe9"
     end
   end
 
