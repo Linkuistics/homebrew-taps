88658f0f460cb2ed13d8d59c8c13cac4f002ecaf
Antony Blakey <antony.blakey@gmail.com>
2026-05-26 18:23:07 +1000

    grove v1.0.0

--- Formula/grove.rb
+++ Formula/grove.rb
@@ -1,24 +1,24 @@
 class Grove < Formula
   desc "Hierarchical, self-extending workstream tool for AI agents"
   homepage "https://github.com/Linkuistics/grove"
-  version "0.1.0"
+  version "1.0.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/Linkuistics/grove/releases/download/v0.1.0/grove-darwin-arm64.tar.gz"
-      sha256 "54584e2e146131820d69973ced2687478e83b20c175443650d19f58b29c84351"
+      url "https://github.com/Linkuistics/grove/releases/download/v1.0.0/grove-v1.0.0-aarch64-apple-darwin.tar.xz"
+      sha256 "a475c5ed1f244d7436f848f76704b911f482bec22f2eee03386bc12d49a7f232"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/Linkuistics/grove/releases/download/v0.1.0/grove-linux-arm64.tar.gz"
-      sha256 "6b31478337ec15a4adbef5634e2676dce9da557ad2105bd78318e8f140459823"
+      url "https://github.com/Linkuistics/grove/releases/download/v1.0.0/grove-v1.0.0-aarch64-unknown-linux-gnu.tar.xz"
+      sha256 "da64111d6ddda70c7a5e8d90aa91e34ac0abe8720cea0f996f7fda61cede64b3"
     end
     on_intel do
-      url "https://github.com/Linkuistics/grove/releases/download/v0.1.0/grove-linux-x86_64.tar.gz"
-      sha256 "e7bd6cd359002d417da8e83732cc13d6269afacde5b90272b30c2880a5937534"
+      url "https://github.com/Linkuistics/grove/releases/download/v1.0.0/grove-v1.0.0-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "4b6403b53838053e9daae39f63413290296428e03bb3b86c5fad47f6ac6fc91f"
     end
   end
 
@@ -27,6 +27,6 @@ class Grove < Formula
   end
 
   test do
-    assert_match "grove cli 0.1.0", shell_output("#{bin}/grove version")
+    assert_match "grove cli 1.0.0", shell_output("#{bin}/grove version")
   end
 end
