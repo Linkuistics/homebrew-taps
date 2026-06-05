fa93270cd2c717c804d51dc3562a364adeb3478a
Antony Blakey <antony.blakey@gmail.com>
2026-06-05 19:23:48 +1000

    grove v6.1.0

--- Formula/grove.rb
+++ Formula/grove.rb
@@ -1,24 +1,24 @@
 class Grove < Formula
   desc "Hierarchical, self-extending workstream tool for AI agents"
   homepage "https://github.com/Linkuistics/grove"
-  version "6.0.0"
+  version "6.1.0"
   license "Apache-2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/Linkuistics/grove/releases/download/v6.0.0/grove-v6.0.0-aarch64-apple-darwin.tar.xz"
-      sha256 "a4bc68396003ad42867360a8fd1d141c10deeb357ad7925e3f7400351c07ec8e"
+      url "https://github.com/Linkuistics/grove/releases/download/v6.1.0/grove-v6.1.0-aarch64-apple-darwin.tar.xz"
+      sha256 "9da36ed310a0915793b20c26e4c11bea5c1f86f7818d9696a7c84d821958067f"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/Linkuistics/grove/releases/download/v6.0.0/grove-v6.0.0-aarch64-unknown-linux-gnu.tar.xz"
-      sha256 "6a1ba4cb77ceba309b9d9b752e9272df1144b4e04baa433bd7a621be6b45d5ea"
+      url "https://github.com/Linkuistics/grove/releases/download/v6.1.0/grove-v6.1.0-aarch64-unknown-linux-gnu.tar.xz"
+      sha256 "dc526e1d6981da72035adc6ab877f87c96b7dfb073c001228ab3da14c0157d0b"
     end
     on_intel do
-      url "https://github.com/Linkuistics/grove/releases/download/v6.0.0/grove-v6.0.0-x86_64-unknown-linux-gnu.tar.xz"
-      sha256 "357762ef31705e5477981ed3883705984bc735483865d6da2587c549986ba95a"
+      url "https://github.com/Linkuistics/grove/releases/download/v6.1.0/grove-v6.1.0-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "d681cc401c9fbeb2ac9bf3dd0d76669f9e8c34ab3524292811e9f2d18a362bac"
     end
   end
 
@@ -27,7 +27,7 @@ class Grove < Formula
   end
 
   test do
-    assert_match "grove 6.0.0", shell_output("#{bin}/grove --version")
-    assert_match "grove-llm 6.0.0", shell_output("#{bin}/grove-llm --version")
+    assert_match "grove 6.1.0", shell_output("#{bin}/grove --version")
+    assert_match "grove-llm 6.1.0", shell_output("#{bin}/grove-llm --version")
   end
 end
